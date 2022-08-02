50e5c6295a0b86fdc750b2f12f6ee09be0918361
TaiLinhPN <linh.nguyenbuitai23@student.passerellesnumeriques.org>
2022-08-02 13:24:25 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SecondComponent from './Second';
+
+function App() {
+  return (
+    <div className="App">
+      <SecondComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Second.js
@@ -0,0 +1,10 @@
+function SecondComponent() {
+    return (
+        <div>
+            <div>he lo hé lồ</div>
+            {/* <SecondComponent /> */}
+        </div>
+    )
+}
+
+export default SecondComponent;
